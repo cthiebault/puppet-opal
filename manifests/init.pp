0179a6f5562ea6fd700a927e1680ff6d64394c12
Cedric Thiebault <cedric.thiebault@nespresso.com>
2014-11-10 16:11:27 +0100

    Configure Mysql databases

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,14 @@
-class opal {
+class opal (
+  $mysql_user = 'opaluser',
+  $mysql_password = 'opalpass',
+) {
 
-  class { 'java':
-    distribution => 'jre',
-  }
+#  class { 'java':
+#    distribution => 'jre',
+#  }
 
-  class { '::mysql::server':
-    override_options => {
-      'mysqld' => {
-        'default-storage-engine' => 'INNODB',
-        'innodb_file_per_table' => true,
-        'init_connect' => 'SET collation_connection = utf8_bin',
-        'init_connect' => 'SET NAMES utf8',
-        'character-set-server' => 'utf8',
-        'collation-server' => 'utf8_bin',
-        'skip-character-set-client-handshake' => true,
-        'max_allowed_packet' => '1G',
-      }
-    }
-  }
+  include '::mongodb::server'
+
+  include 'opal::mysql'
 
 }
---
+++ manifests/mysql.pp
@@ -0,0 +1,39 @@
+class opal::mysql (
+  $mysql_user = 'opaluser',
+  $mysql_password = 'opalpass',
+) {
+
+  class { '::mysql::server':
+    override_options => {
+      'mysqld' => {
+        'default-storage-engine' => 'INNODB',
+        'innodb_file_per_table' => true,
+        'init_connect' => 'SET collation_connection = utf8_bin',
+        'init_connect' => 'SET NAMES utf8',
+        'character-set-server' => 'utf8',
+        'collation-server' => 'utf8_bin',
+        'skip-character-set-client-handshake' => true,
+        'max_allowed_packet' => '1G',
+      }
+    }
+  }
+
+  mysql::db { 'opal_data':
+    user     => $mysql_user,
+    password => $mysql_password,
+    host     => 'localhost',
+    grant    => 'ALL',
+    charset  => 'utf8',
+    collate  => 'utf8_bin',
+  }
+
+  mysql::db { 'opal_ids':
+    user     => $mysql_user,
+    password => $mysql_password,
+    host     => 'localhost',
+    grant    => 'ALL',
+    charset  => 'utf8',
+    collate  => 'utf8_bin',
+  }
+
+}
